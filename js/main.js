9d20abacd044f8f127c8bd17fb0b2ddc8d81f482
MikeGalli <mikegalligar@Mikes-MacBook-Pro-2.local>
2016-05-01 21:34:09 -0700

    fdffffe

--- js/main.js
+++ js/main.js
@@ -1,68 +1,46 @@
-/* Begin by adding your on ready handler here, and then create the
-   rest of your functions inside the on ready handler.
-
-   (Note that you do not need to manually call Bootstrap functions in
-   your Javascript because Bootstrap will automatically recognize your
-   HTML structures and invoke the proper JS code accordingly. Be sure
-   to reference the Bootstrap documentation.)
-*/
 $(document).ready(function(){
 
 
 
-var mymap = L.map('map-container').setView([46.852, -121.760], 13);
+  var mymap = L.map('map-container').setView([46.852, -121.760], 13);
 
-var mapStuff = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-    maxZoom: 18,
+  var mapStuff = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+      maxZoom: 18,
 
-}).addTo(mymap);
+  }).addTo(mymap);
 
+  var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+      subdomains: ['otile1','otile2','otile3','otile4']
+  });
 
+  var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
+      subdomains: ['otile1','otile2','otile3','otile4']
+  });
 
-var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['otile1','otile2','otile3','otile4']
-});
+  var mapLayers = {
+      "Satellite": satLayer,
+      "Map View": drawLayer,
+      "Open Street Maps": mapStuff
+  }
+  L.control.layers(mapLayers).addTo(mymap);
+  satLayer.addTo(mymap);
 
-var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['otile1','otile2','otile3','otile4']
-});
+  var marker = L.marker([46.852, -121.760]).addTo(mymap);
+  marker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 14,411 feet high.");
 
-var mapLayers = {
-    "Satellite": satLayer,
-    "Map View": drawLayer,
-    "Open Street Maps": mapStuff
-}
-L.control.layers(mapLayers).addTo(mymap);
-satLayer.addTo(mymap);
+  var marker2 = L.marker([46.853441, -121.789265]).addTo(mymap);
+  marker2.bindPopup("<b>Let's name all the rocks!</b><br>This one's named for St Andrew");
 
-var marker = L.marker([46.852, -121.760]).addTo(mymap);
-marker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
+  var marker3 = L.marker([46.862922, -121.774825]).addTo(mymap);
+  marker3.bindPopup("<b>Liberty Cap!</b><br>Lowest of the three summits");
 
-var marker2 = L.marker([46.853441, -121.789265]).addTo(mymap);
-marker2.bindPopup("<b>Let's name all the rocks!</b><br>This one's named for St Andrew");
+  var circle = L.circle([46.852, -121.760], 50, {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5
+  }).addTo(mymap);
 
-var marker3 = L.marker([46.862922, -121.774825]).addTo(mymap);
-marker3.bindPopup("<b>Liberty Cap!</b><br>Lowest of the three summits");
-
-var circle = L.circle([46.852, -121.760], 50, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5
-}).addTo(mymap);
-
-
-// TODO: Inside of your on ready handler, invoke the Leaflet.js library
-// to draw a map in your `#map-container` div.
-
-// TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
-// tiles served through the MapQuest CDN. Consult this example to set up
-// the map tiles layers:
-
-
-// TODO: Customize that Map to show markers with popups at no fewer than 3
-// interesting locations. (You'll need to figure out the latitude/longitude for
-// these locations using a mapping tool such as Google Maps.)
 });
