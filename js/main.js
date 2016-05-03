5176837329f40ed7404302ec9d8a2a15448014fd
MikeGalli <mikegalligar@Mikes-MacBook-Pro-2.local>
2016-05-03 14:24:20 -0700

    lo[i

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(".carousel-caption").css("size", "400px");
+  $(".carousel-caption").css("size", "900px");
 
 
   var mymap = L.map('map-container').setView([46.852, -121.760], 12);
